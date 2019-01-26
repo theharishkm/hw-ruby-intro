c62e291a60fc7fff29cb6e9d43f765428ae43852
theharishkm <theharishkm@gmail.com>
2019-01-25 22:45:35 -0600

    Update ruby_intro.rb
    Part1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,14 +3,50 @@
 # Part 1
 
 def sum arr
+  array_size={arr.size}
+  array_sum=0
+  while array_size>=0
+    array_sum=array_sum+arr[array_size]
+    array_size=array_size-1
+  end
+  return array_sum
   # YOUR CODE HERE
 end
 
 def max_2_sum arr
+  arr.sort
+  array_size1={arr.size}
+  if array_size1>=1
+    array_sum1=arr[array_size1]-arr[array_size1-1]
+  elsif array_size1=0
+    array_sum1=arr[array_size1]
+  else
+    array_sum1=0
+  end
+  return array_sum1
   # YOUR CODE HERE
 end
 
 def sum_to_n? arr, n
+  size={arr.size}
+  if size<0
+    return false
+  else
+    while size>=0
+     size1={arr.size}
+     comparator=arr[size]+arr[size1]
+     while n!=comparator
+       if n==comparator
+         return true
+       elsif size1>0
+         size1=size1-1        
+       else
+         break
+       end
+     end
+     size=size-1
+    end
+    return false
   # YOUR CODE HERE
 end
 
